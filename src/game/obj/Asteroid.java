e07480e3a76861fbf06dbb6b7520c4e290d5270c
danhws <92162835+danhws@users.noreply.github.com>
2022-04-03 13:15:35 +0100

    Asteroid Creation Fixed

--- src/game/obj/Asteroid.java
+++ src/game/obj/Asteroid.java
@@ -4,7 +4,7 @@ package game.obj;
 import game.AsteroidsGame;
 import game.Obj;
 import java.awt.Polygon;
-import java.awt.Rectangle;
+//import java.awt.Rectangle;
 
 /**
  *
@@ -18,33 +18,37 @@ public class Asteroid extends Obj {
     
     // size = 1 small, 2 medium, 3 large
     public Asteroid(AsteroidsGame game, double x, double y, int size) {
-        super(game);
+        super(game); //calls parent class constructor
         this.x = x;
         this.y = y;
         this.size = size;
         this.halfSize = size * 10;
+
         angle = (2 * Math.PI) * Math.random();
-        double v = 0.5 + 1 * Math.random();
-        vx = (4 - size) * Math.cos(angle) * v;
-        vy = (4 - size) * Math.sin(angle) * v;
-        va = 0.01 + 0.05 * Math.random();
+        double v = 0.5 + 1 * Math.random(); //velocity factor
+
+        vx = (4 - size) * Math.cos(angle) * v; //horizontal speed
+        vy = (4 - size) * Math.sin(angle) * v; //vertical speed
+        va = 0.01 + 0.05 * Math.random(); //spin speed
+
         //setShape();
         generateRandomShape();
     }
     
-    private void setShape() {
-        //Ellipse2D asteroidShape = new Ellipse2D.Double(-halfSize, -halfSize, 2 * halfSize, 2 * halfSize);
-        Rectangle.Double asteroidShape = new Rectangle.Double(-halfSize, -halfSize, 2 * halfSize, 2 * halfSize);
-        shape = asteroidShape;
-    }
+    // private void setShape() {
+    //     //Ellipse2D asteroidShape = new Ellipse2D.Double(-halfSize, -halfSize, 2 * halfSize, 2 * halfSize);
+    //     Rectangle.Double asteroidShape = new Rectangle.Double(-halfSize, -halfSize, 2 * halfSize, 2 * halfSize);
+    //     shape = asteroidShape;
+    // }
 
     private void generateRandomShape() {
         Polygon randomAsteroidShape = new Polygon();
-        int f = 5 + (int) (5 * Math.random());
+        int f = 5 + (int) (5 * Math.random());//random number of sides
         double da = (2 * Math.PI) / f;
         double a = (2 * Math.PI) * Math.random();
+
         for (int i = 0; i < f; i++) {
-            double ad = halfSize + halfSize * Math.random();
+            double ad = (1.5 * halfSize) + (halfSize * Math.random());
             double ax = ad * Math.cos(a);
             double ay = ad * Math.sin(a);
             randomAsteroidShape.addPoint((int) ax, (int) ay);
@@ -67,11 +71,16 @@ public class Asteroid extends Obj {
     }
 
     public void hit() {
-        if (size > 1) {
+        //if the asteroid is the largest size, create two medium size asteroids
+        if (size == 3) {
             game.add(new Asteroid(game, x, y, size - 1));
+            game.add(new Asteroid(game, x, y, size - 1));
+        //if the asteroid is medium size, create two small size asteroids
+        } else if (size == 2) {
             game.add(new Asteroid(game, x, y, size - 1));
             game.add(new Asteroid(game, x, y, size - 1));
         }
+        //destroy current asteroid
         destroyed = true;
     }
     
